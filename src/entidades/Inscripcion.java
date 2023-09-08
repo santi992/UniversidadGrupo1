68b82936cb3b2cf653b7accbed0a41171d94bce7
Gaby1981 <gabyamestoy@gmail.com>
2023-09-08 12:09:54 -0300

    Clases

--- src/entidades/Inscripcion.java
+++ src/entidades/Inscripcion.java
@@ -10,4 +10,65 @@ package entidades;
  */
 public class Inscripcion {
     
+    private int idInscripto;
+    private int nota;
+    private Alumno alumno;
+    private Materia materia;
+
+    public Inscripcion(int idInscripto, int nota, Alumno alumno, Materia materia) {
+        this.idInscripto = idInscripto;
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion(int nota, Alumno alumno, Materia materia) {
+        this.nota = nota;
+        this.alumno = alumno;
+        this.materia = materia;
+    }
+
+    public Inscripcion() {
+    }
+
+    @Override
+    public String toString() {
+        return "Inscripcion{" + "idInscripto=" + idInscripto + ", nota=" + nota + ", alumno=" + alumno + ", materia=" + materia + '}';
+    }
+
+    public int getIdInscripto() {
+        return idInscripto;
+    }
+
+    public void setIdInscripto(int idInscripto) {
+        this.idInscripto = idInscripto;
+    }
+
+    public int getNota() {
+        return nota;
+    }
+
+    public void setNota(int nota) {
+        this.nota = nota;
+    }
+
+    public Alumno getAlumno() {
+        return alumno;
+    }
+
+    public void setAlumno(Alumno alumno) {
+        this.alumno = alumno;
+    }
+
+    public Materia getMateria() {
+        return materia;
+    }
+
+    public void setMateria(Materia materia) {
+        this.materia = materia;
+    }
+    
+      
+    
+    
 }
--- src/entidades/Materia.java
+++ src/entidades/Materia.java
@@ -10,4 +10,63 @@ package entidades;
  */
 public class Materia {
     
+    private int idMateria;
+    private String nombre;
+    private int anio;
+    private boolean activo;
+
+    public Materia(int idMateria, String nombre, int anio, boolean activo) {
+        this.idMateria = idMateria;
+        this.nombre = nombre;
+        this.anio = anio;
+        this.activo = activo;
+    }
+
+    public Materia(String nombre, int anio, boolean activo) {
+        this.nombre = nombre;
+        this.anio = anio;
+        this.activo = activo;
+    }
+
+    public Materia() {
+    }
+
+    @Override
+    public String toString() {
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", anio=" + anio + ", activo=" + activo + '}';
+    }
+
+    public int getIdMateria() {
+        return idMateria;
+    }
+
+    public void setIdMateria(int idMateria) {
+        this.idMateria = idMateria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+    
+    
 }
